2619c63ce52047c719b0cd16d3d85cc2374a87a0
Joris Mak <joris@aubergine-it.nl>
2020-03-11 15:37:14 +0100

    revert commit that attempts to use global object

--- src/auth.js
+++ src/auth.js
@@ -4,7 +4,7 @@ import createAuthRefreshInterceptor from "axios-auth-refresh";
 // We need to save the credentials in this variable,
 // because the contents of tableau.password will be empty
 // after accessing it the first time.
-global.savedCredentials = global.savedCredentials || null;
+let savedCredentials;
 
 createAuthRefreshInterceptor(axios, () => refreshAndSaveCredentials());
 
@@ -20,7 +20,7 @@ export function getApiKey({ APIKey } = getCredentials()) {
 
 export function saveCredentials(credentials = getCredentials()) {
     tableau.password = JSON.stringify(credentials);
-    global.savedCredentials = credentials;
+    savedCredentials = credentials;
 }
 
 export function getCredentials() {
